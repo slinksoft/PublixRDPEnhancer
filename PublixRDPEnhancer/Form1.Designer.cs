3f3528fa19768970c1f7f4159526eb6e152927da
unknown <dbruc009@fiu.edu>
2022-09-01 09:51:56 -0400

    Initial commit

---
+++ PublixRDPEnhancer/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace PublixRDPEnhancer
+{
+    partial class mCanvas
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mCanvas));
+            this.enableMic = new System.Windows.Forms.CheckBox();
+            this.enableMultiMonitor = new System.Windows.Forms.CheckBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.publixTitle = new System.Windows.Forms.RichTextBox();
+            this.warning = new System.Windows.Forms.Label();
+            this.versionDisplay = new System.Windows.Forms.Label();
+            this.aboutButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkRDP = new System.Windows.Forms.Timer(this.components);
+            this.estSessionDis = new System.Windows.Forms.Label();
+            this.statusLabelDis = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enableMic
+            // 
+            this.enableMic.AutoSize = true;
+            this.enableMic.Checked = true;
+            this.enableMic.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.enableMic.Location = new System.Drawing.Point(56, 69);
+            this.enableMic.Name = "enableMic";
+            this.enableMic.Size = new System.Drawing.Size(166, 19);
+            this.enableMic.TabIndex = 0;
+            this.enableMic.Text = "Enable Remote Mic Device";
+            this.enableMic.UseVisualStyleBackColor = true;
+            // 
+            // enableMultiMonitor
+            // 
+            this.enableMultiMonitor.AutoSize = true;
+            this.enableMultiMonitor.Checked = true;
+            this.enableMultiMonitor.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.enableMultiMonitor.Location = new System.Drawing.Point(70, 94);
+            this.enableMultiMonitor.Name = "enableMultiMonitor";
+            this.enableMultiMonitor.Size = new System.Drawing.Size(134, 19);
+            this.enableMultiMonitor.TabIndex = 1;
+            this.enableMultiMonitor.Text = "Enable Dual Monitor";
+            this.enableMultiMonitor.UseVisualStyleBackColor = true;
+            // 
+            // connectButton
+            // 
+            this.connectButton.BackgroundImage = global::PublixRDPEnhancer.Properties.Resources.preconnectButton;
+            this.connectButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.connectButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.connectButton.Location = new System.Drawing.Point(87, 115);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(101, 89);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // publixTitle
+            // 
+            this.publixTitle.BackColor = System.Drawing.SystemColors.Control;
+            this.publixTitle.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.publixTitle.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.publixTitle.ForeColor = System.Drawing.Color.Green;
+            this.publixTitle.Location = new System.Drawing.Point(58, 8);
+            this.publixTitle.Name = "publixTitle";
+            this.publixTitle.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.publixTitle.Size = new System.Drawing.Size(166, 22);
+            this.publixTitle.TabIndex = 5;
+            this.publixTitle.Text = "Publix RDP Enhancer";
+            // 
+            // warning
+            // 
+            this.warning.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.warning.Location = new System.Drawing.Point(13, 247);
+            this.warning.Name = "warning";
+            this.warning.Size = new System.Drawing.Size(251, 191);
+            this.warning.TabIndex = 6;
+            this.warning.Text = resources.GetString("warning.Text");
+            this.warning.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // versionDisplay
+            // 
+            this.versionDisplay.AutoSize = true;
+            this.versionDisplay.Location = new System.Drawing.Point(204, 456);
+            this.versionDisplay.Name = "versionDisplay";
+            this.versionDisplay.Size = new System.Drawing.Size(66, 15);
+            this.versionDisplay.TabIndex = 7;
+            this.versionDisplay.Text = "Version: 0.0";
+            // 
+            // aboutButton
+            // 
+            this.aboutButton.Location = new System.Drawing.Point(3, 429);
+            this.aboutButton.Name = "aboutButton";
+            this.aboutButton.Size = new System.Drawing.Size(87, 42);
+            this.aboutButton.TabIndex = 8;
+            this.aboutButton.Text = "What Does This Do?";
+            this.aboutButton.UseVisualStyleBackColor = true;
+            this.aboutButton.Click += new System.EventHandler(this.aboutButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(27, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 67);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "By: SlinkSoft - A Publix Software Engineer";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkRDP
+            // 
+            this.checkRDP.Enabled = true;
+            this.checkRDP.Interval = 500;
+            this.checkRDP.Tick += new System.EventHandler(this.checkRDP_Tick);
+            // 
+            // estSessionDis
+            // 
+            this.estSessionDis.Font = new System.Drawing.Font("Segoe UI", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.estSessionDis.ForeColor = System.Drawing.Color.Black;
+            this.estSessionDis.Location = new System.Drawing.Point(14, 228);
+            this.estSessionDis.Name = "estSessionDis";
+            this.estSessionDis.Size = new System.Drawing.Size(248, 19);
+            this.estSessionDis.TabIndex = 10;
+            this.estSessionDis.Text = "N/A";
+            this.estSessionDis.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // statusLabelDis
+            // 
+            this.statusLabelDis.AutoSize = true;
+            this.statusLabelDis.Font = new System.Drawing.Font("Segoe UI", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.statusLabelDis.ForeColor = System.Drawing.Color.Black;
+            this.statusLabelDis.Location = new System.Drawing.Point(113, 207);
+            this.statusLabelDis.Name = "statusLabelDis";
+            this.statusLabelDis.Size = new System.Drawing.Size(49, 19);
+            this.statusLabelDis.TabIndex = 11;
+            this.statusLabelDis.Text = "Status";
+            this.statusLabelDis.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mCanvas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 477);
+            this.Controls.Add(this.statusLabelDis);
+            this.Controls.Add(this.estSessionDis);
+            this.Controls.Add(this.aboutButton);
+            this.Controls.Add(this.versionDisplay);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.warning);
+            this.Controls.Add(this.publixTitle);
+            this.Controls.Add(this.enableMultiMonitor);
+            this.Controls.Add(this.enableMic);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(292, 516);
+            this.MinimumSize = new System.Drawing.Size(292, 516);
+            this.Name = "mCanvas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Publix RDP Enhancer - By SlinkSoft";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CheckBox enableMic;
+        private CheckBox enableMultiMonitor;
+        private Button connectButton;
+        private RichTextBox publixTitle;
+        private Label warning;
+        private Label versionDisplay;
+        private Button aboutButton;
+        private Label label2;
+        private System.Windows.Forms.Timer checkRDP;
+        private Label estSessionDis;
+        private Label statusLabelDis;
+    }
+}
---
+++ PublixRDPEnhancer/Form1.cs
@@ -0,0 +1,160 @@
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace PublixRDPEnhancer
+{
+    public partial class mCanvas : Form
+    {
+        #region Retrieve Known Folder Logic
+        public enum KnownFolder
+        {
+            Downloads
+        }
+
+        public static class KnownFolders
+        {
+            private static readonly Dictionary<KnownFolder, Guid> _guids = new()
+            {
+                [KnownFolder.Downloads] = new("374DE290-123F-4565-9164-39C4925E467B")
+            };
+
+            public static string GetPath(KnownFolder knownFolder)
+            {
+                return SHGetKnownFolderPath(_guids[knownFolder], 0);
+            }
+
+            [DllImport("shell32",
+                CharSet = CharSet.Unicode, ExactSpelling = true, PreserveSig = false)]
+            private static extern string SHGetKnownFolderPath(
+                [MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags,
+                nint hToken = 0);
+        }
+        #endregion
+
+        string downloadPath = KnownFolders.GetPath(KnownFolder.Downloads);
+        string[] rdpFiles;
+
+        int ver, rev;
+
+        public mCanvas()
+        {
+            InitializeComponent();
+
+            ver = 1;
+            rev = 0;
+            versionDisplay.Text = "Version: " + ver + "." + rev;
+
+            // title logic
+            publixTitle.Text = "";
+            AppendText(publixTitle, "Publix ", System.Drawing.Color.Green);
+            AppendText(publixTitle, "RDP ", System.Drawing.Color.Blue);
+            AppendText(publixTitle, "Enhancer", System.Drawing.Color.Black);
+
+        }
+        private void doEnhancedConnect()
+        {
+
+            // Will take all .RDP files in your Downloads directory and delete all of them except the latest one (if only one exists, nothing happens)
+            DirectoryInfo downloadsDirectory = new DirectoryInfo(downloadPath);
+            var query = downloadsDirectory.GetFiles("*.rdp", SearchOption.TopDirectoryOnly);
+            foreach (var file in query.OrderByDescending(file => file.CreationTime).Skip(1))
+            {
+                file.Delete();
+            }
+
+            rdpFiles = Directory.GetFiles(downloadPath, "*.rdp", SearchOption.TopDirectoryOnly);
+
+
+            // Check to ensure the only RDP file left is the latest one downloaded from Publix RDP
+            if (rdpFiles.Length == 1)
+            {
+                // Read contents of RDP file
+                StreamReader read = new StreamReader(rdpFiles[0]);
+                string currLine = "";
+                string rdpContents = "";
+                while ((currLine = read.ReadLine()) != null)
+                {
+                    // We can set User-desired Settings here so we can save on additional logic when writing back to the file
+
+                    // Remote Mic Device Option
+                    if (currLine.Contains("audiocapturemode:i:"))
+                    {
+                        if (enableMic.Checked)
+                            currLine = currLine.Substring(0, currLine.Length - 1) + "1";
+                        else
+                            currLine = currLine.Substring(0, currLine.Length - 1) + "0";
+
+                    }
+
+                    // Dual Monitor Option
+                    if (currLine.Contains("use multimon:i:"))
+                    {
+                        if (enableMultiMonitor.Checked)
+                            currLine = currLine.Substring(0, currLine.Length - 1) + "1";
+                        else
+                            currLine = currLine.Substring(0, currLine.Length - 1) + "0";
+                    }
+                        
+
+                    rdpContents += currLine + "\n";
+                }
+                read.Close();
+               
+                // Write updated contents to RDP file
+                StreamWriter write = new StreamWriter(rdpFiles[0]);
+                write.Write(rdpContents);
+                write.Close();
+
+                // Open and Establish RDP session
+
+                Process.Start(@"C:\Windows\System32\mstsc.exe", rdpFiles[0]);
+                
+            }
+            else
+            {
+                MessageBox.Show("Old RDP File Deletion Did Not Run, No RDP File Found or Another Error Occurred! Delete All RDP files in your Download directory" +
+                    " and try again.", "Error");
+            }
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            doEnhancedConnect();
+        }
+
+        private void aboutButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Properties.Resources.aboutInfo, "About The Publix RDP Enhancer");
+        }
+
+        private void checkRDP_Tick(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName("mstsc");
+
+            // the RDP process is not opened / running
+            if (processes.Length == 0)
+            {
+                this.connectButton.BackgroundImage = global::PublixRDPEnhancer.Properties.Resources.preconnectButton;
+                estSessionDis.Text = Properties.Resources.notConnected;
+                estSessionDis.ForeColor = System.Drawing.Color.Black;
+            }
+            else // process is running
+            {
+                this.connectButton.BackgroundImage = global::PublixRDPEnhancer.Properties.Resources.connectButton;
+                estSessionDis.Text = Properties.Resources.sessionEst;
+                estSessionDis.ForeColor = System.Drawing.Color.Green;
+            }
+        }
+
+        public void AppendText(RichTextBox box, string text, Color color)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.SelectionColor = box.ForeColor;
+        }
+    }
+}
